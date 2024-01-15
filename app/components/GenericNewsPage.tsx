17aa8d2511a7fa4c1a569ec167d364933d4f2785
Kadin Vo <kadinvo@gmail.com>
2024-01-15 14:15:28 +1300

    added rest of categories

--- app/components/GenericNewsPage.tsx
+++ app/components/GenericNewsPage.tsx
@@ -66,6 +66,10 @@ const GenericNewsPage = () => {
             <option value="business">Business</option>
             <option value="entertainment">Entertainment</option>
             <option value="general">General</option>
+            <option value="health">Health</option>
+            <option value="science">Science</option>
+            <option value="sports">Sports</option>
+            <option value="technology">Technology</option>
           </select>
         </div>
       </div>
